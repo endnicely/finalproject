2d81acb474c94ee336d00638f3e55fcf35c940ed
endnicely <zhoujie66@yahoo.com>
2018-05-07 16:05:52 -0700

    CalculateTotal, confirmation modal and show&hide billing info

--- js/script.js
+++ js/script.js
@@ -14,6 +14,29 @@ function toggleOptions(toggle) {
     
 }
 
+function calculateTotal(){
+    "use strict";
+    //window.alert(document.getElementsByClassName('input[class="toppings"]').length);
+    
+    var count = 0;
+    var toppings = document.getElementsByClassName("toppings");
+    //window.alert(toppings.length);
+    for (var i=0; i< toppings.length; i++) {       
+       if (toppings[i].checked == true){
+          count++;
+       }
+    }
+    //window.alert(document.getElementsByClassName('input[class="toppings"]:checked').length);
+    window.alert($("sizeCost").value);
+    window.alert($("optCheese").value);
+    //var total = parseFloat($("sizeCost").value) + parseFloat($("optCheese").value) + parseFloat($("optSauce").value) + count * 0.99 ;
+    var total = Number($("sizeCost").value) + Number($("optCheese").value) + Number($("optSauce").value) + count * 0.99 ;
+//    var total = parseFloat(($("sizeCost").value).toFixed(2)) + parseFloat(($("optCheese").value).toFixed(2)) +   parseFloat(($("optSauce").value).toFixed(2)) + count * 0.99 ;
+  
+    window.alert(total);
+    $("total").value = total;
+}
+
 window.addEventListener("load", function () {
     "use strict";
      var doughSizePrize = {
@@ -32,6 +55,7 @@ window.addEventListener("load", function () {
      
     var optdoughlist =  document.getElementsByName('optdough');
     var optdoughItems = [].slice.call(optdoughlist);
+    $("billingInfo").style.display ="none";
    
 //  hide the input for other address type initially
     $("otherAddressType").style.display = "none";
@@ -57,13 +81,38 @@ window.addEventListener("load", function () {
             //var opt = doughSizePrize[item.id][i];
             var el = document.createElement("option");
             el.textContent = selectedDough[i].size +  " (" + selectedDough[i].price + ")";
-            el.value = selectedDough[i].size +  selectedDough[i].price;
+            //el.value = selectedDough[i].size +  selectedDough[i].price;
+            el.value = selectedDough[i].price.substr(1);
             $('sizeCost').appendChild(el);
         }
         toggleOptions(false);
+        //$("total").value = $("sizeCost").value;
+        calculateTotal();
        
     });
    });
+   $("sizeCost").addEventListener("change", calculateTotal);
+   $("optCheese").addEventListener("change", calculateTotal);
+   $("optSauce").addEventListener("change", calculateTotal);
+   var optToppingsList =  document.getElementsByClassName('toppings');
+   var optToppingsItems = [].slice.call(optToppingsList);
+    optToppingsItems.forEach(function (item) {
+        "use strict";
+        item.addEventListener('change',function(){
+            calculateTotal();
+        });
+    });
+     $("proceedToCheckout").addEventListener("click", function(){
+        $("Confirmation_buildingPizza").style.display = "none";
+        //$("Confirmation_buildingPizza").setAttribute("data-dismiss", "modal");
+        $("billingInfo").style.display = "block"; 
+     });
+   //$("toppings").addEventListener("click", calculateTotal);
+//    buttons.addEventListener("click", function (e) {
+//	window.console.log(e.target.innerText);
+//});
+
+    
 });
 
 
