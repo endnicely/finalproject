9463401ab2dd3715b67a13c00ee2e48eb7405266
endnicely <zhoujie66@yahoo.com>
2018-05-07 11:31:05 -0700

    populate size and cost dropdownlist based on Dough selection
    populate size and cost dropdownlist based on Dough selection and disable cheese/sauce option and toppings initially

--- js/script.js
+++ js/script.js
@@ -4,8 +4,35 @@ var $ = function (id) {
     return window.document.getElementById(id);
 };
 
+function toggleOptions(toggle) {
+    $("optCheese").disabled = toggle;
+    $("optSauce").disabled = toggle;
+    var toppings =  document.getElementsByClassName("toppings");
+    for (var j=0; j <  toppings.length; j++) {
+        toppings[j].disabled = toggle;
+    }
+    
+}
+
 window.addEventListener("load", function () {
     "use strict";
+     var doughSizePrize = {
+                         handTossed:[{size:"Small", price:"$9.99"},
+                                     {size:"Medium", price:"$12.99"},
+                                     {size:"Large", price:"$14.99"},
+                         ],
+                         thinCrust:[{size:"Medium", price:"$11.99"},
+                                     {size:"Large", price:"$13.99"},
+                         ],
+                         newYorkStyle:[{size:"Large", price:"$16.99"},
+                                     {size:"Extra Large", price:"$19.99"},
+                         ],
+                         glutenFree:[{size:"Small", price:"$10.99"}]                                
+                         }
+     
+    var optdoughlist =  document.getElementsByName('optdough');
+    var optdoughItems = [].slice.call(optdoughlist);
+   
 //  hide the input for other address type initially
     $("otherAddressType").style.display = "none";
     
@@ -17,7 +44,26 @@ window.addEventListener("load", function () {
             $("otherAddressType").style.display = "none";
         }
     });
-   
+    toggleOptions(true);
+    //window.alert( document.getElementsByClassName("toppings").length);
+    optdoughItems.forEach(function (item) {
+        "use strict";
+        item.addEventListener('change',function(){
+      
+        var selectedDough = doughSizePrize[item.id];
+        //window.alert(selectedDough.length);
+        $('sizeCost').innerHTML = null;
+        for(var i = 0; i < selectedDough.length; i++) {
+            //var opt = doughSizePrize[item.id][i];
+            var el = document.createElement("option");
+            el.textContent = selectedDough[i].size +  " (" + selectedDough[i].price + ")";
+            el.value = selectedDough[i].size +  selectedDough[i].price;
+            $('sizeCost').appendChild(el);
+        }
+        toggleOptions(false);
+       
+    });
+   });
 });
 
 
