71e992a3ac15b7c5e858d4baf865e9fb5a9dacb6
endnicely <zhoujie66@yahoo.com>
2018-05-11 15:58:32 -0700

    Validation Billing Inoformation incompleted

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ function toggleOptions(toggle) {
 
 //utility function
 function ReadOnlyForm(formId, isReadOnly) {
+    "use strict";
     var f = document.forms[formId];
     for(var i=0,fLen=f.length;i<fLen;i++) {
       f.elements[i].readOnly = isReadOnly;  //the "O" must be upper case
@@ -24,6 +25,7 @@ function ReadOnlyForm(formId, isReadOnly) {
 }
 
 function validateInput(inputName, isValid, feedback) {
+    "use strict";
     if(isValid!=="unknown"){
         var removeClass = isValid ? "is-invalid" : "is-valid";
         var addClass = isValid ? "is-valid" : "is-invalid";
@@ -52,14 +54,8 @@ function validateInput(inputName, isValid, feedback) {
           $(inputName).nextElementSibling.classList.remove("is-valid");
           $(inputName).nextElementSibling.innerHTML = feedback;
         }
-    }
-   
-    
-    
-      
+    }  
 }
-
-
 
 function isValidDeliveryForm() {
     "use strict";
@@ -137,10 +133,12 @@ function isValidDeliveryForm() {
 }
 
 function isInputEmpty(input) {
+    "use strict";
     return $(input).value.trim() === "" ? true : false;
 }
 
 function isValidOrderForm() {
+    "use strict";
     if ($("total").value !== ""){
         return true;
     }else {
@@ -150,31 +148,37 @@ function isValidOrderForm() {
 }
 
 function isValidFullName(fullName) {
+    "use strict";
     return /^[a-z]+ [a-z]+$/i.test(fullName);
     //return /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g.test(fullName);
 }
 
 function isValidAddress(address) {
+    "use strict";
     //return /^[-.?!,;:() A-Za-z0-9]*$/.test(address);
     return /\d+[ ](?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?/.test(address);
 }
 
 function isValidCity(city) {
+    "use strict";
     return /(?:[A-Z][a-z.-]+[ ]?)+/.test(city);
 }
 
 function isValidState(state) {
+    "use strict";
     var objRegExp = /^(AK|AL|AR|AZ|CA|CO|CT|DC|DE|FL|GA|HI|IA|ID|IL|IN|KS|KY|LA|MA|MD|ME|MI|MN|MO|MS|MT|NB|NC|ND|NH|NJ|NM|NV|NY|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VA|VT|WA|WI|WV|WY)$/i; 
     return objRegExp.test(state);
 }
 
 
 function isValidUSZip(sZip) {
+    "use strict";
     return /^\d{5}(-\d{4})?$/.test(sZip);
     //\b\d{5}(?:-\d{4})?\b
 }
 
 function isValidPhoneNumber(phoneNo) {
+    "use strict";
     //valid phone number pattern
     //'123-345-3456';
     //'(078)789-8908';
@@ -184,14 +188,17 @@ function isValidPhoneNumber(phoneNo) {
 }
 
 function isValidEmail(email) {
+    "use strict";
     return /\S+@\S+\.\S+/.test(email);
 }
 
 function isValidCVV(cvv) {
+    "use strict";
     return /^[0-9]{3,4}$/.test(cvv);
 }
 
-function isValidExpirationDate(month,year){ 
+function isValidExpirationDate(month,year){
+    "use strict";
     var cur_year = new Date().getFullYear();
 
     if(cur_year == parseInt(year,10)) {     
@@ -257,7 +264,7 @@ window.addEventListener("load", function () {
         }
     });
     toggleOptions(true);
-    //window.alert( document.getElementsByClassName("toppings").length);
+ 
     optdoughItems.forEach(function (item) {
         "use strict";
         item.addEventListener('change',function(){
@@ -292,7 +299,8 @@ window.addEventListener("load", function () {
         });
     });
     
-    $("btnFinishBuildPizza").addEventListener("click",function(e){
+    $("btnFinishBuildPizza").addEventListener("click",function(e) {
+        "use strict";
         if (this.innerHTML.trim() === "Finished Building Pizza"){
             if((!isValidDeliveryForm()) || (!isValidOrderForm())) {
                 e.stopImmediatePropagation();      
@@ -308,6 +316,7 @@ window.addEventListener("load", function () {
     });
     
     $("proceedToCheckout").addEventListener("click", function(){
+        "use strict";
         $("Confirmation_buildingPizza").style.display = "none";
         $("billingInfo").style.display = "block";
         ReadOnlyForm("deliveryLocation", true);
@@ -317,6 +326,7 @@ window.addEventListener("load", function () {
     });
     
     $("name").addEventListener("blur", function(e){
+        "use strict";
         if(isValidFullName(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -325,6 +335,7 @@ window.addEventListener("load", function () {
     });
     
     $("stAddress").addEventListener("blur", function(e){
+        "use strict";
         if(isValidAddress(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -333,6 +344,7 @@ window.addEventListener("load", function () {
     });
     
     $("city").addEventListener("blur", function(e){
+        "use strict";
         if(isValidCity(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -341,6 +353,7 @@ window.addEventListener("load", function () {
     });
     
     $("state").addEventListener("blur", function(e){
+        "use strict";
         if(isValidState(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -349,6 +362,7 @@ window.addEventListener("load", function () {
     });
     
     $("zipcode").addEventListener("blur", function(e){
+        "use strict";
         if(isValidUSZip(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -357,6 +371,7 @@ window.addEventListener("load", function () {
     });
     
     $("phoneno").addEventListener("blur", function(e){
+        "use strict";
         if(isValidPhoneNumber(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -365,6 +380,7 @@ window.addEventListener("load", function () {
     });
     
     $("email").addEventListener("blur", function(e){
+        "use strict";
         if(isValidEmail(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -373,6 +389,7 @@ window.addEventListener("load", function () {
     });
     
     $("sameAsDeli").addEventListener("click", function(){
+        "use strict";
         if(this.checked) {
            $("cardHolderName").value = $("name").value;
            $("bStAddress").value = $("stAddress").value;
@@ -384,6 +401,7 @@ window.addEventListener("load", function () {
     });
     
     $("cardHolderName").addEventListener("blur", function(e){
+        "use strict";
         if(isValidFullName(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -391,6 +409,7 @@ window.addEventListener("load", function () {
         }
     });
     $("bStAddress").addEventListener("blur", function(e){
+        "use strict";
         if(isValidAddress(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -399,6 +418,7 @@ window.addEventListener("load", function () {
     });
     
     $("bCity").addEventListener("blur", function(e){
+        "use strict";
         if(isValidCity(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -406,6 +426,7 @@ window.addEventListener("load", function () {
         }
     });
     $("bState").addEventListener("blur", function(e){
+        "use strict";
         if(isValidState(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -414,6 +435,7 @@ window.addEventListener("load", function () {
     });
     
     $("bZipCode").addEventListener("blur", function(e){
+        "use strict";
         if(isValidUSZip(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -422,6 +444,7 @@ window.addEventListener("load", function () {
     });
    
     $("cvv").addEventListener("blur", function(e){
+        "use strict";
         if(isValidCVV(e.currentTarget.value)) {
             validateInput(e.currentTarget.id, true, "looks good");   
         } else {
@@ -430,6 +453,7 @@ window.addEventListener("load", function () {
     });
     
      $("expiryMonth").addEventListener("blur", function(e){
+         "use strict";
          if(isInputEmpty(e.currentTarget.id)) {
             validateInput(e.currentTarget.id, false, "Please select a month"); 
          } else if (isInputEmpty("expiryYear")) { 
@@ -451,6 +475,7 @@ window.addEventListener("load", function () {
     });
     
      $("expiryYear").addEventListener("blur", function(e){
+         "use strict";
          if(isInputEmpty(e.currentTarget.id)) {
             validateInput(e.currentTarget.id, false, "Please select a year"); 
          } else if (isInputEmpty("expiryMonth")) { 
@@ -466,6 +491,7 @@ window.addEventListener("load", function () {
     });
     
     $("expiryYear").addEventListener("blur", function(e){
+         "use strict";
          if(isInputEmpty(e.currentTarget.id)) {
             validateInput(e.currentTarget.id, false, "Please select a year"); 
          } else if (isInputEmpty("expiryMonth")) { 
@@ -484,33 +510,228 @@ window.addEventListener("load", function () {
         if(cardNo!==""){
             var valid1stDigits = ["4","5","3"];
             var validPrefix2Digits = ["40","41", "42", "43", "44", "45", "46", "47", "48", "49","51","52","53","54","55","37"];
-            var stringfyCardNo = cardNo.toString();
-            if(stringfyCardNo.length === 1){
-                return valid1stDigits.indexOf(stringfyCardNo[0]) > -1;
+            //var stringfyCardNo = cardNo.toString();
+            if(cardNo.length === 1){
+                return valid1stDigits.indexOf(cardNo[0]) > -1;
             }
             else {
-                return validPrefix2Digits.indexOf(stringfyCardNo.substr(0,2)) > -1 ;
+                return validPrefix2Digits.indexOf(cardNo.substr(0,2)) > -1 ;
             }
             
         } else {
             return false;
         }  
     }
+    function isNotNum(cardNo) {
+        "use strict";
+        var lastDigit = cardNo.slice(-1);
+        return isNaN(parseInt(lastDigit,10));        
+    }
+    function isValidCardLength(cardNo) {
+        "use strict";
+        var carPrefix = parseInt(cardNo.substr(0,2), 10);
+        switch(carPrefix) {
+            case 40:
+            case 41:
+            case 42:
+            case 43:
+            case 44:
+            case 45:
+            case 46:
+            case 47:
+            case 48:
+            case 49:
+                if(cardNo.length === 13 || cardNo.length === 16) {
+                    return true;
+                }
+                break;
+            case 51:
+            case 52:
+            case 53:
+            case 54:
+            case 55:
+                if(cardNo.length === 16) {
+                    return true;
+                }
+                break;
+            case 37:
+                if(cardNo.length === 15) {
+                    return true;
+                }
+                break;
+            default:
+                return false;
+        }
+    }
+    
+    function isValidCreditCard(value) {
+        "use strict";
+        if(isNotNum(value) && isCardNoValidPrefix(value) && isValidCardLength(value)) {
+            var cDigit="", nCheck = 0, nDigit = 0, bEven = false;  
+            for (var n = value.length - 1; n >= 0; n--) {
+            cDigit = value.charAt(n);
+            nDigit = parseInt(cDigit, 10);
+
+            if (bEven) {
+                if ((nDigit *= 2) > 9) {                  
+                    nDigit = nDigit.toString();
+                }
+            }
+            
+            if(typeof nDigit === 'string' || nDigit instanceof String) { 
+
+                nCheck += parseInt(nDigit[0], 10) + parseInt(nDigit[1], 10);
+            }
+            else {
+                nCheck += nDigit;
+               
+            }
+         
+            bEven = !bEven;
+        }
+            return (nCheck % 10) == 0;
+        } else {
+            return false;
+        }
+    }
+    
+    function creditCardType(cardNo){
+        "use strict";
+        switch(cardNo.substr(0,1)) {
+            case "4":
+                return "Visa";
+            case "5":
+                return "Master";
+            case "3":       
+                return "American Express";
+            default:
+                break;
+        }
+    }
+    
+    function isValidBillingForm() {
+    "use strict";
+    var isValid = false;
+    var inputs = document.forms["billingInfo"].querySelectorAll("input[type=text]");
+    var selects = document.forms["billingInfo"].getElementsByTagName('select');
+    for (var i = 0; i< inputs.length; i++) {
+        if(isInputEmpty(inputs[i].id)) {
+            validateInput(inputs[i].id, false, inputs[i].name + " is required");
+        } else {
+             switch(inputs[i].id) {
+                    case "cardHolderName":
+                        isValid = isValidFullName($(inputs[i].id).value);
+                        break;
+                    case "cardNumber":
+                        isValid = isValidCreditCard($(inputs[i].id).value);
+                        break;
+                    case "bStAddress":
+                        isValid = isValidAddress($(inputs[i].id).value);
+                        break;
+                    case "bCity":
+                        isValid = isValidCity($(inputs[i].id).value);
+                        break;
+                    case "bState":
+                        isValid = isValidState($(inputs[i].id).value);
+                        break;
+                    case "bZipCode":
+                        isValid = isValidUSZip($(inputs[i].id).value);
+                        break;
+                    case "cvv":
+                        isValid = isValidCVV($(inputs[i].id).value);
+                        break;
+                    default:
+            }
+            isValid ? validateInput(inputs[i].id, true, "looks good") : validateInput(inputs[i].id, false, inputs[i].name + " is invalid");
+        }
+       
+    }
+        
+    var message="";
+    for (var j = 0; j< selects.length; j++) {
+        if(isInputEmpty(selects[j].id)) {
+            message += selects[j].name + " ";
+            validateInput(selects[j].id, false, message + "is required");
+        } else {
+           if(j !== 0) {
+               isValid = isValidExpirationDate(selects[j-1],selects[j]);
+           }  
+        }
+        isValid ? validateInput(selects[j].id, true, "looks good") : validateInput(selects[j].id, false, selects[j].name + " is invalid");
+    }
+    return isValid;
+        
+//        if(isInputEmpty(arrRequired[i])) {
+//            validateInput(arrRequired[i], false, $(arrRequired[i]).name + " is required");
+//            isValid = false;
+//        } else {
+//                switch(arrRequired[i]) {
+//                    case "name":
+//                        isValid = isValidFullName($(arrRequired[i]).value);
+//                        break;
+//                    case "addressType":
+//                        if($(arrRequired[i]).value === "other" && isInputEmpty("otherAddressType") ) {
+//                             validateInput("otherAddressType", false, $("otherAddressType").value + "is required");
+//                            isValid = false;
+//                        } else if ($(arrRequired[i]).value === "apartment" && isInputEmpty("suiteno") ) {
+//                             validateInput("suiteno", false, $("suiteno").value + "is required");
+//                        }
+//                        break;
+//                    case "stAddress":
+//                        isValid = isValidAddress($(arrRequired[i]).value);
+//                        break;
+//                    case "city":
+//                        isValid = isValidCity($(arrRequired[i]).value);
+//                        break;
+//                    case "state":
+//                        isValid = isValidState($(arrRequired[i]).value);
+//                        break;
+//                    case "zipcode":
+//                        isValid = isValidUSZip($(arrRequired[i]).value);
+//                        break;
+//                    case "phoneon":
+//                        isValid = isValidPhoneNumber($(arrRequired[i]).value);
+//                        break;
+//                    case "email":
+//                        isValid = isValidEmail($(arrRequired[i]).value);
+//                        break;
+//                    default:
+//                }
+//                isValid ? validateInput(arrRequired[i], true, "looks good") : validateInput(arrRequired[i], false, $(arrRequired[i]).name + " is invalid");
+//        }
+//    }
+//    return isValid;    
+}
     $("cardNumber").addEventListener("keyup",function(e){
-           "use strict";
-           this.value.replace(/\s/g, "");
-           var cardNumber = parseInt(e.currentTarget.value,10);
-           
-           if (isNaN(cardNumber)) {
-             validateInput(e.currentTarget.id, false, "Credit Card can't contain non numeric character!");
-           }else if (!isCardNoValidPrefix(cardNumber)){
-              validateInput(e.currentTarget.id, false, "Sorry! Only Visa, MasterCard or American Express are accepted"); 
-           }else{
-              validateInput(e.currentTarget.id, "unknown", "");
-           }
-                                     
+       "use strict";
+       var cardNo = e.currentTarget.value;
+       var creditCardInputId = e.currentTarget.id;
+       if (isNotNum(cardNo)) {
+          validateInput(creditCardInputId, false, "Credit Card can't contain non numeric character!");
+          //not allow space and other non number typing in credit card
+          this.value = this.value.slice(0, -1);
+
+       } else if (!isCardNoValidPrefix(cardNo)){
+          validateInput(creditCardInputId, false, "Sorry! Only Visa(4), MasterCard(51,52,53,54,55) or American Express(37) are accepted");
+          //not allow typing unacceptable card prefix in credit card
+          this.value = this.value.slice(0, -1);
+       }else if (!(isValidCardLength(cardNo))){
+          validateInput(creditCardInputId, false, "Valid number of digits for Visa (13 or 16), MasterCard(16), American Express(15)");
+       }else if(!isValidCreditCard(cardNo)){
+          validateInput(creditCardInputId, false, "Invalid Card Number");
+       }else{
+          validateInput(creditCardInputId, true, "Your " + creditCardType(cardNo) + " Card looks good");
+       }                            
     });
     
+     $("btnPay").addEventListener("click", function(){
+        "use strict";
+        if (isValidBillingForm()) {
+            window.location.href = "confirmation.html";
+        } else {
+            return;
+        }
+    });
     
 });
 
